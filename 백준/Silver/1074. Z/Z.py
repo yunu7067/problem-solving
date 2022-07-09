2f6a96a6ed5054a9cd661bf70ce689803c034c9d
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-07-09 21:15:02 +0900

    [Silver I] Title: Z, Time: 116 ms, Memory: 113248 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,26 +1,16 @@
 import sys
 
-dr = [0, 0, 1, 1]
-dc = [0, 1, 0, 1]
-
-def Z(n, s, p):
+def Z(n, p):
     if n == 0:
-        print(s)
-        return
-    next_n = n - 1
-    next_half = pow(2, next_n)
-
-    for i in range(4):
-        next_r = dr[i] * next_half
-        next_c = dc[i] * next_half
-
-        if (dr[i] * next_half <= p[0] < next_half * (1 + dr[i])) and (
-            dc[i] * next_half <= p[1] < next_half * (1 + dc[i])
-        ):
-            next_p = (p[0] - next_r, p[1] - next_c)
-            next_s = s + next_half * next_half * (2 * dr[i] + 1 * dc[i])
-            Z(next_n, next_s, next_p)
-
+        return 0
+    next_half = 1 << n - 1
+    if p[0] < next_half and p[1] < next_half:
+        return Z(n - 1, (p[0], p[1]))
+    if p[0] < next_half and p[1] >= next_half:
+        return 1 * next_half * next_half + Z(n - 1, (p[0], p[1] - next_half))
+    if p[0] >= next_half and p[1] < next_half:
+        return 2 * next_half * next_half + Z(n - 1, (p[0] - next_half, p[1]))
+    return 3 * next_half * next_half + Z(n - 1, (p[0] - next_half, p[1] - next_half))
 
 N, r, c = map(int, sys.stdin.readline().split())
-Z(N, 0, (r, c))
+print(Z(N, (r, c)))
