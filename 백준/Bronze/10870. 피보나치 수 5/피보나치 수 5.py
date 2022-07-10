acfbf9f44c2babef2de064f1cec5c0ae9629bb7b
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-07-10 21:37:41 +0900

    [Bronze V] Title: 피보나치 수 5, Time: 112 ms, Memory: 113248 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,9 @@
+import sys
+
+def Fibonacci(n: int, pp: int, p: int):
+    if n <= 0:
+        return pp + p
+    return Fibonacci(n - 1, p, pp + p)
+
+N = int(sys.stdin.readline().rstrip())
+print(Fibonacci(N - 2, 0, 1) if N != 0 else 0)
