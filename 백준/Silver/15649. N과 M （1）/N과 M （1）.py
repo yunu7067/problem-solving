b334be6f255906f930604e17d5b111ae9aecfc8e
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-07-11 16:35:22 +0900

    [Silver III] Title: N과 M (1), Time: 276 ms, Memory: 123488 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,17 +1,19 @@
-from itertools import permutations
+from re import A
 import sys
-from typing import List
-
-def NM(M: int, nums: List, p: List):
-    if M <= 0:
-        print(" ".join(map(str, p)))
-        return
-    for num in nums:
-        next_nums, next_p = nums.copy(), p.copy()
-        next_nums.remove(num)
-        next_p.append(num)
-        NM(M - 1, next_nums, next_p)
 
 N, M = map(int, sys.stdin.readline().split())
-nums = list(range(1, N + 1))
-NM(M, nums, [])
+arr = [0] * M
+is_used = [False] * N
+
+def NM(k: int):
+    if k == M:
+        print(" ".join(map(str, arr)))
+        return
+    for num in range(0, N):
+        if not is_used[num]:
+            arr[k] = num + 1
+            is_used[num] = True
+            NM(k + 1)
+            is_used[num] = False
+
+NM(0)
