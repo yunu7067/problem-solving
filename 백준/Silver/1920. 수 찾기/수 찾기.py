90dd935e3dc6892f152d94665b68b5614a6e455c
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-09-18 16:53:25 +0900

    [Silver IV] Title: 수 찾기, Time: 256 ms, Memory: 140540 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,27 +1,20 @@
 import sys
 
 def bisect(a, target):
-    start, mid, end = 0, len(a) // 2, len(a)
-    if a[end - 1] < target:
-        return 0
-
-    while True:
-        if a[mid] == target:
-            return 1
-        elif a[mid] < target:
-            start = mid + 1
-            if mid == end:
-                break
-        else:
-            end = mid
-            if start == mid:
-                break
+    start, end = 0, len(a) - 1
+    while start <= end:
         mid = (start + end) // 2
+        if a[mid] < target:
+            start = mid + 1
+        elif a[mid] > target:
+            end = mid - 1
+        else:
+            return 1
     return 0
 
 N = int(sys.stdin.readline().rstrip())
 A = list(sorted(list(map(int, sys.stdin.readline().split()))))
 M = int(sys.stdin.readline().rstrip())
-B = list(map(int, sys.stdin.readline().split()))
+B = map(int, sys.stdin.readline().split())
 for t in B:
     print(bisect(A, t))
