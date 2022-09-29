40b5683004d4f65358f5f48b82367f536ab5071a
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-09-29 16:01:21 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -2,35 +2,29 @@ from heapq import heappop, heappush
 
 def solution(operations):
     garbage = [False] * (len(operations) + 1)
-    length = 0
     h, desc_h = [], []
+
     for (idx, operation) in enumerate(operations):
         op, value = operation.split(" ")
         value = int(value)
         if op == "I":
             heappush(h, (value, idx))
             heappush(desc_h, (-value, idx))
-            length += 1
         else:
-            if length == 0:
-                continue
-            if value == 1:
-                cur_v, cur_i = heappop(desc_h)
-                while garbage[cur_i]:
-                    cur_v, cur_i = heappop(desc_h)
-            else:
-                cur_v, cur_i = heappop(h)
-                while garbage[cur_i]:
-                    cur_v, cur_i = heappop(h)
-            length -= 1
-            garbage[cur_i] = True
-
-    if length == 0:
-        return [0, 0]
+            if value == 1 and desc_h:
+                _, cur_i = heappop(desc_h)
+                while desc_h and garbage[cur_i]:
+                    _, cur_i = heappop(desc_h)
+                garbage[cur_i] = True
+            elif value == -1 and h:
+                _, cur_i = heappop(h)
+                while h and garbage[cur_i]:
+                    _, cur_i = heappop(h)
+                garbage[cur_i] = True
 
     while h and garbage[h[0][1]]:
         heappop(h)
     while desc_h and garbage[desc_h[0][1]]:
         heappop(desc_h)
 
-    return [-desc_h[0][0], h[0][0]]
+    return [-desc_h[0][0], h[0][0]] if h and desc_h else [0, 0]
