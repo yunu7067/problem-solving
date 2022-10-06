ae44fc7033e0aedf80d9a81da9468c206e984fdd
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-10-07 02:40:33 +0900

    [Silver IV] Title: 듣보잡, Time: 164 ms, Memory: 125400 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,18 +1,9 @@
 import sys
-
 input = sys.stdin.readline
+
 N, M = map(int, input().rstrip().split())
-unheard, both = {}, []
-
-for _ in range(N):
-    unheard[input().rstrip()] = True
-
-for _ in range(M):
-    name = input().rstrip()
-    if name in unheard:
-        both.append(name)
-
+unheard = set([input().rstrip() for _ in range(N)])
+unseen = set([input().rstrip() for _ in range(M)])
+both = list(unheard & unseen)
 both.sort()
-print(len(both))
-for name in both:
-    print(name)
+print(len(both), *both, sep="\n")
