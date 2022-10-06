1f19260b11156d88f232fb479f16b5d18c4a6a06
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-10-07 02:23:30 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 352 ms, Memory: 145376 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,18 +1,13 @@
-# from collections import defaultdict, deque
-from heapq import heappop, heappush
 import sys
-from typing import List
 
 input = sys.stdin.readline
 N, M = map(int, input().rstrip().split())
 pedia = {}
 
-
 for idx in range(1, N + 1):
     name = input().rstrip()
-    pedia[name] = idx
-    pedia[str(idx)] = name
+    pedia[name], pedia[idx] = idx, name
 
 for _ in range(M):
     q = input().rstrip()
-    print(pedia[q])
+    print(pedia[int(q)] if q.isdigit() else pedia[q])
