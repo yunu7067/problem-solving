46b1c46632a75a1908789c2d31e99c3e5f7588ca
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-10-09 19:07:35 +0900

    [Silver III] Title: 프린터 큐, Time: 152 ms, Memory: 115636 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,31 +1,22 @@
-from collections import deque, Counter
+from collections import deque
 import sys
-
-# from heapq import heappop, heappush
-
 input = sys.stdin.readline
-T = int(input())
 
+T = int(input())
 for _ in range(T):
     N, M = map(int, input().split())
+    docs = deque(map(int, input().split()))
+    idxs = deque(range(N))
     count = 0
-    docs = list(map(int, input().split()))
-    priorities, counter = list(set(docs)), Counter(docs)
-    priorities.sort(reverse=True)
-    priorities = deque(priorities)
-    Q = deque([[index, priority] for index, priority in enumerate(docs)])
 
     while True:
-        idx, pri = Q.popleft()
-        if priorities.index(pri) > 0:
-            Q.append([idx, pri])
-            continue
-        else:
-            counter[pri] -= 1
-            if counter[pri] == 0:
-                priorities.popleft()
+        if docs[0] == max(docs):
+            docs.popleft()
+            cur_idx = idxs.popleft()
             count += 1
-        if idx == M:
-            break
-
-    print(count)
+            if cur_idx == M:
+                print(count)
+                break
+        else:
+            docs.append(docs.popleft())
+            idxs.append(idxs.popleft())
