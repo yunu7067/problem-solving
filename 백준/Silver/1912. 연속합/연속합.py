4061efabf50a8fe35dae5158a2ee139e7a5ddbb0
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-10-17 00:54:08 +0900

    [Silver II] Title: 연속합, Time: 128 ms, Memory: 127968 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+data = list(map(int, input().split()))
+
+for i in range(1, n):
+    data[i] = max(data[i], data[i] + data[i - 1])
+
+print(max(data))
