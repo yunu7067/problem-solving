3bc952c79480d99e3d298c9de0830db4f5a3e917
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-11-05 19:26:17 +0900

    [Gold II] Title: 보석 도둑, Time: 2220 ms, Memory: 183856 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,23 +1,20 @@
 from heapq import heappush, heappop
 import sys
-
 input = sys.stdin.readline
+
 N, K = map(int, input().split())
-gems, bags = [], []
+gems = []
 for _ in range(N):
     heappush(gems, list(map(int, input().split())))
-for _ in range(K):
-    heappush(bags, int(input()))
+bags = [int(input()) for _ in range(K)]
+bags.sort()
 
 total, gems_sorted_by_value = 0, []
-for _ in range(K):
-    cur_bag = heappop(bags)
-    while gems and gems[0][0] <= cur_bag:
-        gem = heappop(gems)
+for bag in bags:
+    while gems and gems[0][0] <= bag:
         # 가치가 큰 순으로 저장 (-가치, 무게)
-        heappush(gems_sorted_by_value, (-gem[1], gem[0]))
+        heappush(gems_sorted_by_value, -heappop(gems)[1])
     if gems_sorted_by_value:
-        most_expensive_gem = heappop(gems_sorted_by_value)
-        total += -most_expensive_gem[0]
+        total += -heappop(gems_sorted_by_value)
 
 print(total)
