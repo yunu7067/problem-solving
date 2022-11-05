9a1de64059af5177dd55bc719f3b641918d1c8ec
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2022-11-05 17:54:20 +0900

    [Gold IV] Title: 단어 수학, Time: 140 ms, Memory: 114792 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.py
+++ 백준/Gold/1339. 단어 수학/단어 수학.py
@@ -1,23 +1,18 @@
 from collections import defaultdict
 import sys
+
 input = sys.stdin.readline
-
-N = int(input())
-WORDS = [input().rstrip() for _ in range(N)]
-count = defaultdict(int)
-
-for _i in range(65, 91):
-    target = chr(_i)
-    for word in WORDS:
-        target_num = 0
+count, summ = defaultdict(int), 0
+for _ in range(int(input())):
+    word = input().rstrip()
+    chars = list(set(word))
+    for char in chars:
         for idx, s in enumerate(word):
-            if s == target:
-                target_num += 10 ** (len(word) - idx - 1)
-        count[target] += target_num
+            if s == char:
+                count[char] += 10 ** (len(word) - idx - 1)
+
 count_list = list(count.items())
 count_list.sort(key=lambda x: x[1], reverse=True)
-
-summ = 0
-for num in range(9, -1, -1):
-    summ += count_list[9 - num][1] * num
+for i, c in enumerate(count_list[:9]):
+    summ += c[1] * (9 - i)
 print(summ)
