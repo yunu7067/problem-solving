a2122fd79313b326af9860cfc94a897e9889dfad
최윤혁 <57068092+yunu7067@users.noreply.github.com>
2024-05-28 17:51:48 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 129.52 ms, Memory: 58.7 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,16 +1,7 @@
 def solution(sequence):
-    seqPositive = []
-    for a, b in enumerate(sequence):
-        pos = 1 if (a % 2) == 0 else -1
-        seqPositive.append(b * pos)
+    summax = [0]   
+    for i in range(len(sequence)):
+        pulse = 1 if i % 2 == 0 else -1
+        summax.append(summax[-1] + pulse * sequence[i])
     
-    summax, summin = [None] * len(sequence), [None] * len(sequence)
-    summax[0] = seqPositive[0]
-    summin[0] = seqPositive[0]
-    
-    for i in range(1, len(sequence)):
-        summax[i] = max(0, summax[i-1]) + seqPositive[i]
-        summin[i] = min(0, summin[i-1]) + seqPositive[i]
-    maxsum = max(max(summax), -1 * min(summin))
-    
-    return maxsum
+    return abs(max(summax) - min(summax))
